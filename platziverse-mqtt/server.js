d26b1fac24d007be805889f034c571a5061853ab
jefferlindao <jefferlindao@gmail.com>
2021-04-30 13:46:21 -0500

    Como recibir mensaje

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -15,9 +15,32 @@ const settings = {
   port: 1883,
   backend
 }
-
 const server = new mosca.Server(settings)
+
+server.on('clientConnected', client => {
+  debug(`Client Connected: ${client.id}`)
+})
+
+server.on('clientDisconnected', client => {
+  debug(`CLient Disconnected: ${client.id}`)
+})
+
+server.on(`published`, (packet, client) => {
+  debug(`Received: ${packet.topic}`)
+  debug(`Payload: ${packet.payload}`)
+})
 
 server.on('ready', () => {
   console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
 })
+
+server.on('error', handlerFatalError)
+
+function handlerFatalError(err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handlerFatalError)
+process.on('unhandledRejection', handlerFatalError)
