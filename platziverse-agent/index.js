699934ce406d35f4c766b93119ebc0af59572b97
jefferlindao <jefferlindao@gmail.com>
2021-05-01 18:35:05 -0500

    Implementacion del cliente MQTT

--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -1,27 +1,68 @@
 'use strict'
+const debug = require('debug')
+const mqtt = require('mqtt')
+const defaults = require('defaults')
 const EventEmitter = require('events')
+const { parsePayload } = require('./util')
+const uuid = require('uuid')
+
+const options = {
+  name: 'untitled',
+  username: 'platzi',
+  interval: 5000,
+  mqtt: {
+    host: 'mqtt://localhost'
+  }
+}
 class PlatziverseAgeny extends EventEmitter {
-  constructor (opts) {
+  constructor(opts) {
     super()
 
-    this._options = opts
+    this._options = defaults(opts, options)
     this._started = null
     this._timer = null
+    this._client = null
   }
 
-  connect () {
+  connect() {
     if (!this._started) {
+      const opts = this._options
+      this._client = mqtt.connect(opts.mqtt.host)
       this._started = true
 
-      this.emit('connected')
-      const opts = this._options
-      this._timer = setInterval(() => {
-        this.emit('agent/message', 'this is a message')
-      }, opts.interval)
+      this.client.subscribe('agent/message')
+      this.client.subscribe('agent/connected')
+      this.client.subscribe('agent/disconnected')
+
+      this._client.on('connect', () => {
+        this._agentId = uuid.v4()
+        this.emit('connected', this._agentId)
+
+        this._timer = setInterval(() => {
+          this.emit('agent/message', 'this is a message')
+        }, opts.interval)
+      })
+
+      this._client.on('message', (topic, payload) => {
+        payload = parsePayload(payload)
+        let broadcast = false
+        switch (topic) {
+          case 'agent/connected':
+          case 'agent/disconnected':
+          case 'agent/message':
+            broadcast = payload && payload.agent && payload.agent.uuid !== this._agentId
+            break
+        }
+        if (broadcast) {
+          this.emit(topic, payload)
+        }
+      })
+
+      this._client.on('error', () => this.disconnec())
     }
   }
 
-  disconnec () {
+  disconnec() {
     if (this._started) {
       clearInterval(this._timer)
       this._started = false
