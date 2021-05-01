8b261ddeebead30de4b667c62c9456fa55df8d13
jefferlindao <jefferlindao@gmail.com>
2021-05-01 18:00:31 -0500

    Definiendo un timer de ejecucion continua (setInterval)

--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -1,9 +1,33 @@
 'use strict'
 const EventEmitter = require('events')
 class PlatziverseAgeny extends EventEmitter {
-  constructor() {
+  constructor (opts) {
     super()
+
+    this._options = opts
+    this._started = null
+    this._timer = null
+  }
+
+  connect () {
+    if (!this._started) {
+      this._started = true
+
+      this.emit('connected')
+      const opts = this._options
+      this._timer = setInterval(() => {
+        this.emit('agent/message', 'this is a message')
+      }, opts.interval)
+    }
+  }
+
+  disconnec () {
+    if (this._started) {
+      clearInterval(this._timer)
+      this._started = false
+      this.emit('disconnnected')
+    }
   }
 }
 
-module.exports = PlatziverseAgeny;
+module.exports = PlatziverseAgeny
