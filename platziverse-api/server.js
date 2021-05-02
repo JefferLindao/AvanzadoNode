578d7f366d2a58e5559e1a2313811709573d20f1
jefferlindao <jefferlindao@gmail.com>
2021-05-02 11:21:48 -0500

    Definicion de rutas del API

--- platziverse-api/server.js
+++ platziverse-api/server.js
@@ -5,7 +5,10 @@ const express = require('express')
 const chalk = require('chalk')
 const port = process.env.PORT || 3000
 const app = express()
+const api = require('./api')
 const server = http.createServer(app)
+
+app.use('/api', api)
 
 server.listen(port, () => {
   console.log(`${chalk.green('[plaziverse-api]')} server listening on port ${port}`)
