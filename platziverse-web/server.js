90436e89ad0836e8519626419dd542f856b2546e
jefferlindao <jefferlindao@gmail.com>
2021-05-11 21:42:22 -0500

    Integrando soket.io con express

--- platziverse-web/server.js
+++ platziverse-web/server.js
@@ -1,17 +1,32 @@
 'use strict'
 
-const debug = require('debug')('platziverse-web')
+const debug = require('debug')('platziverse:web')
 const http = require('http')
 const path = require('path')
 const express = require('express')
+const socketio = require('socket.io')
 const chalk = require('chalk')
 
 const port = process.env.PORT || 8081
 
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+//socket oi / websockets
+io.on('connect', socket => {
+  debug(`Connected ${socket.id}`)
+
+  socket.on('agent/message', payload => {
+    console.log(payload)
+  })
+
+  setInterval(() => {
+    socket.emit('agent/message', { agent: 'xxx-yyy' })
+  }, 2000)
+})
 
 function handlerFatalError(err) {
   console.error(`${chalk.red('[fatal error]')} ${err.message}`)
