5e127b8e0d884cc9e4bb62a5d961d6b3c10f458c
jefferlindao <jefferlindao@gmail.com>
2021-05-15 16:42:19 -0500

    Implementación del layout básico con blessed contrib

--- platziverse-cli/platziverse.js
+++ platziverse-cli/platziverse.js
@@ -1,5 +1,32 @@
 #!/usr/bin/env node
 
 'use strict'
+/* eslint new-cap: "off" */
 
-console.log('Hello platziverse!')
+const blessed = require('blessed')
+const contrib = require('blessed-contrib')
+
+const screen = blessed.screen()
+
+const grid = new contrib.grid({
+  rows: 1,
+  cols: 4,
+  screen
+})
+
+const tree = grid.set(0, 0, 1, 1, contrib.tree, {
+  label: 'Connected Agents'
+})
+
+const line = grid.set(0, 1, 1, 3, contrib.line, {
+  label: 'Metric',
+  showLegentd: true,
+  minY: 0,
+  xPadding: 5
+})
+
+screen.key(['escape', 'q', 'C-c'], (ch, key) => {
+  process.exit(0)
+})
+
+screen.render()
